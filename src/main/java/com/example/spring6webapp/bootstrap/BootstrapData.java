8b9511b99aaa439a27c2791a35968b3e1f5c4763
felipeortega99 <felipeorcas@gmail.com>
2025-03-10 16:12:19 -0600

    Adding publisher relationship

--- src/main/java/com/example/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/com/example/spring6webapp/bootstrap/BootstrapData.java
@@ -50,16 +50,22 @@ public class BootstrapData  implements CommandLineRunner {
         ericSaved.getBooks().add(dddSaved);
         rodSaved.getBooks().add(noEJBSaved);
 
-        authorRepository.save(ericSaved);
-        authorRepository.save(rodSaved);
-
         Publisher publisher = new Publisher();
         publisher.setPublisherName("Test Publisher");
         publisher.setAddress("Av. Jesus 1260");
         publisher.setCity("Zapopan");
         publisher.setState("Jalisco");
         publisher.setZipCode("45138");
-        publisherRepository.save(publisher);
+        Publisher savedPublisher = publisherRepository.save(publisher);
+
+        dddSaved.setPublisher(savedPublisher);
+        noEJB.setPublisher(savedPublisher);
+
+        authorRepository.save(ericSaved);
+        authorRepository.save(rodSaved);
+        bookRepository.save(dddSaved);
+        bookRepository.save(noEJBSaved);
+
 
         System.out.println("In Bootstrap");
         System.out.println("Author Count: " + authorRepository.count());
